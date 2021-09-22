edc0fe28691bf89bbe806442b6dbc747dac8dcb4
piskor <j.piskorski@wasko.pl>
2021-09-22 09:39:07 +0200

    initial commit

---
+++ src/main/java/pl/polsl/fitstat/FitstatApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.fitstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitstatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/polsl/fitstat/FitstatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.fitstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitstatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
